e0db5d0524b47b9b00ee173d5ea2d6adc7aba95e
akash gupta <akash30g@gmail.com>
2016-09-24 22:11:10 +0800

    buttons and lab el name updated
    adding items will be created

--- AkashGuptaA2.py
+++ AkashGuptaA2.py
@@ -20,37 +20,37 @@ class ShoppingList(App):
     def requiredListMain(self):
         count = 0
         total = 0
-        self.root.ids.entriesBox.clear_widgets()
+        self.root.ids.itemDisplay.clear_widgets()
         required_list = sorted(self.list, key=operator.itemgetter(2))
-        self.root.ids.status_label.text = "Click items to mark an item as completed"
+        self.root.ids.instructionMenu.text = "Click items to mark an item as completed"
         for each_item in required_list:
             if "r" in each_item[3]:
                 if int(each_item[2]) == 1:
                     itemButton = Button(text="{} ${}".format(each_item[0], each_item[1]), background_color=[1, 0, 0, 1])
                     itemButton.item = each_item
-                    itemButton.bind(on_release=self.markCompleted)
-                    self.root.ids.entriesBox.add_widget(itemButton)
+                    #itemButton.bind(on_release=self.markCompleted)
+                    self.root.ids.itemDisplay.add_widget(itemButton)
                     count += 1
                     total += float(each_item[1])
                 elif int(each_item[2]) == 2:
                     itemButton = Button(text="{} ${}".format(each_item[0], each_item[1]), background_color=[0, 0, 1, 1])
                     itemButton.item = each_item
-                    itemButton.bind(on_release=self.markCompleted)
-                    self.root.ids.entriesBox.add_widget(itemButton)
+                    #itemButton.bind(on_release=self.markCompleted)
+                    self.root.ids.itemDisplay.add_widget(itemButton)
                     count += 1
                     total += float(each_item[1])
                 else:
                     itemButton = Button(text="{} ${}".format(each_item[0], each_item[1]), background_color=[0, 1, 0, 1])
                     itemButton.item = each_item
-                    itemButton.bind(on_release=self.markCompleted)
-                    self.root.ids.entriesBox.add_widget(itemButton)
+                    #itemButton.bind(on_release=self.markCompleted)
+                    self.root.ids.itemDisplay.add_widget(itemButton)
                     count += 1
                     total += float(each_item[1])
         if count == 0:
-            self.root.ids.top_label.text = "No required items"
-            self.root.ids.entriesBox.clear_widgets()
+            self.root.ids.itemInfo.text = "No required items"
+            self.root.ids.itemDisplay.clear_widgets()
         else:
-            self.root.ids.top_label.text = "Total price expected for {} items: ${}".format(count, total)
+            self.root.ids.itemInfo.text = "Total price expected for {} items: ${}".format(count, total)
 
 
     #def markCompleted
